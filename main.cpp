d533ebf0be6becffeb3710655b8434344cfe4b00
aronpalfalvi <aron.palfalva1@gmail.com>
2026-03-02 16:24:55 +0100

    Upload main.cpp for bugfix

--- main.cpp
+++ main.cpp
@@ -4,31 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // Hibás név: NELEMENTS -> N_ELEMENTS
-    int* b = new int[N_ELEMENTS];
-    // Rossz idézõjel és hiányzó pontosvesszõ
-    std::cout << "1-100 ertekek duplazasa" << std::endl;
-    // Hiányos ciklusfej
-    for (int i = 0; i < N_ELEMENTS; i++)
+    int *b = new int[NELEMENTS];
+    std::cout << '1-100 ertekek duplazasa'
+    for (int i = 0;)
     {
         b[i] = i * 2;
     }
-    // Hibás feltétel (i) és hiányzó pontosvesszõ
-    for (int i = 0; i < N_ELEMENTS; i++)
+    for (int i = 0; i; i++)
     {
-        std::cout << "Ertek: " << b[i] << std::endl;
-    }
+        std::cout << "Ertek:"
+    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // Nincs kezdõérték (0) adva
-    int atlag = 0;
-    // Vesszõ hiba a ciklusban és hiányzó pontosvesszõ a törzsben
-    for (int i = 0; i < N_ELEMENTS; i++)
+    int atlag;
+    for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i];
+        atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-
-    delete[] b; // Memória felszabadítása
     return 0;
 }
